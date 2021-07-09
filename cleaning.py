3d317eb8eed3a03ab9b4f94ee12c43f4c3cad840
cuijmg <jmycui@gmail.com>
2021-07-09 10:07:31 +0200

    filter n save html

--- cleaning.py
+++ cleaning.py
@@ -9,43 +9,80 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 import pandas as pd
+import re
+from flask import Flask
+app = Flask(__name__)
 
 
 csv.field_size_limit(500 * 1024 * 1024)
+
+def grade(keywordlist,text): # word check
+    text = re.sub('\\.|-|\t|_|%|(|)|％|\(|°|\)|\[|\]|/|“|”|"|"|^|\n','',text)
+    text = re.sub(r'{|}|<|>|-|:|;|,|[.]|[+]','',text)
+    words = re.split(' ',text)
+    points = 0
+    for i in keywordlist:
+        for j in words:
+            if i == j:
+                points+=1
+    return(points)
+
+def score_text(text: str) -> int:
+    words = ''.join(filter(lambda x: x.isalpha() or x.isspace(), text))
+    words = set(words.split(' '))
+    score = 0
+    for keyword in keywords:
+        if keyword in words:
+            score += 1
+    return score
+
+
+def trim_node(root):
+    viable_children = []
+    for child in root.children:
+        if type(child) is not bs4.element.Tag:
+            continue
+        if score_text(child.get_text()) >= THRESHOLD:
+            viable_children.append(child)
+    if len(viable_children) == 0:
+        return None
+    elif len(viable_children) == 1:
+        return trim_node(viable_children[0])
+    else:
+        root['style'] = 'border: 1px solid red;'
+        return root
+    
+keywords=['Datenschutzerklärung','Datenschutz','Datenschutzhinweise',
+          'EU-Datenschutz¬grundverordnung','Datenschutzbeauftragte',
+          'Datenschutzbeauftragter','Datenschutzbeauftragten',
+          'Personenbezogene','Personenbezogener','Daten',
+          'Verarbeitung','personenbezogenen','DSGVO','DS-GVO',
+          'Rechte','Auskunft','Auskunftsrecht','Recht','Analytics']
+
+THRESHOLD = 2
 
 
 import os
 path = r"C:\Users\f-cui\Desktop\ZEW\26" 
 files= os.listdir(path) 
 s = []
-count = 0
+threshhold = 1
 for file in files: 
     print(file)
     if not os.path.isdir(file): 
-        with open(path+"/"+file,'r', encoding='utf-8') as f:
-            reader = csv.reader(f)
-            column = [row[5] for row in reader]
-            # print(column)
-            #  count+=1
-            # print(count)
         
-        for csvinfo in column:
-            if csvinfo != '0':
-                print(csvinfo)
-                print(89)
-                soup = BeautifulSoup(csvinfo, 'lxml')
-#                print(soup.prettify)
-                children = soup.find_all()
-#                children = soup.findChildren()
-#                children = soup.children
-                print(children)
-                print(66)
-                for child in children:
-                    print(child)
-                    print(111)
-                    text = child.get_text()
-                    print(text)
-                    print(222)
+        df = pd.read_csv(path+"/" + file, usecols=['html'], encoding="utf-8")
+        
+      
+        for html in df['html']:
+            csvinfo =str(html)
+            if csvinfo != 'nan':
                 
+                soup = BeautifulSoup(csvinfo, 'lxml')
+                res = trim_node(soup)
+                print(res)
+                f = open(path+"/"+file.strip('.csv')+'.html','w',encoding="utf-8")
+                f.write(str(soup))
+                f.close()
             
     break
