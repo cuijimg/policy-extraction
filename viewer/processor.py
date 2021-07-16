1e8641da8de972767bb9e32e670317f5ade86c34
Cui <F-CUI@zew.local>
2021-07-16 17:20:11 +0200

    mark contact info

--- viewer/processor.py
+++ viewer/processor.py
@@ -5,6 +5,7 @@ Created on Fri Jul  9 16:36:21 2021
 @author: F-CUI
 """
 import bs4
+import re
 
 
 def score_text(text: str) -> int:
@@ -43,18 +44,27 @@ THRESHOLD = 2
 
 
 def process(html):
-    # I know that html is not nan, since I check for that in the calling function.
-    # Therefore, I can simplify this function.
+    # Draw a box around policy content
     soup = bs4.BeautifulSoup(html, 'lxml')
     res = trim_node(soup)
     
+    # Highlight titles
+    # Pattern 1: with the tag 'strong'
     strongcontents = soup.find_all('strong')
     for strongcontent in strongcontents:
         if len(strongcontent.text)>0:
             if strongcontent.text[-1] not in {'?','.'} and strongcontent.text[-2:] not in {'an'}:
                 strongcontent['style'] = 'background-color: yellow; color: black'
     
+    # Pattern 2: with heading tags
     heading_tags = ["h1", "h2", "h3", "h4", "h5", "h6"]
     for tags in soup.find_all(heading_tags):
         tags['style'] = 'background-color: yellow; color: black'
+    
+    # Mark contact information
+    patternlist = ["^\nTel.*","^\nE-Mail.*",".*Straße.*|str.*"]
+    for pattern in patternlist:
+        for ele in soup.find_all(text=re.compile(pattern,re.I)):
+            ele.parent['style'] = 'background-color: blue; color: yellow'
+            # ele.extract()
     return soup
